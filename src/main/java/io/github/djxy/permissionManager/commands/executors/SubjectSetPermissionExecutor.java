261b99d1f1adff0e513f90cb93f5277bb00982e6
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-05-08 10:45:46 -0400

    Commands executors translated

--- src/main/java/io/github/djxy/permissionManager/commands/executors/SubjectSetPermissionExecutor.java
+++ src/main/java/io/github/djxy/permissionManager/commands/executors/SubjectSetPermissionExecutor.java
@@ -1,14 +1,15 @@
 package io.github.djxy.permissionManager.commands.executors;
 
 import io.github.djxy.core.repositories.PlayerRepository;
+import io.github.djxy.permissionManager.Main;
 import io.github.djxy.permissionManager.Permissions;
 import io.github.djxy.permissionManager.commands.CommandExecutor;
 import io.github.djxy.permissionManager.subjects.Permission;
 import io.github.djxy.permissionManager.subjects.Player;
 import io.github.djxy.permissionManager.subjects.Subject;
 import org.spongepowered.api.command.CommandSource;
-import org.spongepowered.api.text.Text;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -28,7 +29,7 @@ public class SubjectSetPermissionExecutor extends CommandExecutor {
         boolean value = (Boolean) values.get("value");
         String name = subject instanceof Player? PlayerRepository.getInstance().getPlayerName(UUID.fromString(subject.getIdentifier())):subject.getIdentifier();
 
-        source.sendMessage(PREFIX.concat(Text.of(INFO_COLOR, name, RESET_COLOR, " has now the permission ", INFO_COLOR, permission, RESET_COLOR, " set to ", INFO_COLOR, value, RESET_COLOR, " globally.")));
+        source.sendMessage(Main.getTranslatorInstance().translate(source, "subjectSetPermission", createMap("subject", name, "permission", permission, "value", value)));
 
         Permission perm = subject.getPermission(permission);
 
@@ -36,6 +37,16 @@ public class SubjectSetPermissionExecutor extends CommandExecutor {
             perm.setValue(value);
         else
             subject.setPermission(new Permission(permission, value));
+    }
+
+    private Map<String,Object> createMap(String key1, Object value1, String key2, Object value2, String key3, Object value3){
+        HashMap<String,Object> map = new HashMap<>();
+
+        map.put(key1, value1);
+        map.put(key2, value2);
+        map.put(key3, value3);
+
+        return map;
     }
 
 }
--- src/main/java/io/github/djxy/permissionManager/commands/executors/SubjectSetPermissionWorldExecutor.java
+++ src/main/java/io/github/djxy/permissionManager/commands/executors/SubjectSetPermissionWorldExecutor.java
@@ -1,15 +1,16 @@
 package io.github.djxy.permissionManager.commands.executors;
 
 import io.github.djxy.core.repositories.PlayerRepository;
+import io.github.djxy.permissionManager.Main;
 import io.github.djxy.permissionManager.Permissions;
 import io.github.djxy.permissionManager.commands.CommandExecutor;
 import io.github.djxy.permissionManager.subjects.Permission;
 import io.github.djxy.permissionManager.subjects.Player;
 import io.github.djxy.permissionManager.subjects.Subject;
 import org.spongepowered.api.command.CommandSource;
-import org.spongepowered.api.text.Text;
 import org.spongepowered.api.world.World;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -30,7 +31,7 @@ public class SubjectSetPermissionWorldExecutor extends CommandExecutor {
         boolean value = (Boolean) values.get("value");
         String name = subject instanceof Player? PlayerRepository.getInstance().getPlayerName(UUID.fromString(subject.getIdentifier())):subject.getIdentifier();
 
-        source.sendMessage(PREFIX.concat(Text.of(INFO_COLOR, name, RESET_COLOR, " has now the permission ", INFO_COLOR, permission, RESET_COLOR, " set to ", INFO_COLOR, value, RESET_COLOR, " in the world ", INFO_COLOR, worldName, RESET_COLOR, ".")));
+        source.sendMessage(Main.getTranslatorInstance().translate(source, "subjectSetPermissionWorld", createMap("subject", name, "permission", permission, "world", worldName, "value", value)));
 
         Permission perm = subject.getPermission(worldName, permission);
 
@@ -38,6 +39,17 @@ public class SubjectSetPermissionWorldExecutor extends CommandExecutor {
             perm.setValue(value);
         else
             subject.setPermission(worldName, new Permission(permission, value));
+    }
+
+    private Map<String,Object> createMap(String key1, Object value1, String key2, Object value2, String key3, Object value3, String key4, Object value4){
+        HashMap<String,Object> map = new HashMap<>();
+
+        map.put(key1, value1);
+        map.put(key2, value2);
+        map.put(key3, value3);
+        map.put(key4, value4);
+
+        return map;
     }
 
 }
