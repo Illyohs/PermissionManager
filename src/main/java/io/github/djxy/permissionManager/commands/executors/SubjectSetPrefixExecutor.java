f5a73f576b1bed5ca6223b3e12afef53fc09c987
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-05-11 10:56:05 -0400

    Translation implemented

--- src/main/java/io/github/djxy/permissionManager/commands/executors/SubjectSetPrefixExecutor.java
+++ src/main/java/io/github/djxy/permissionManager/commands/executors/SubjectSetPrefixExecutor.java
@@ -1,12 +1,13 @@
 package io.github.djxy.permissionManager.commands.executors;
 
+import io.github.djxy.core.CoreUtil;
 import io.github.djxy.core.repositories.PlayerRepository;
+import io.github.djxy.permissionManager.Main;
 import io.github.djxy.permissionManager.Permissions;
 import io.github.djxy.permissionManager.commands.CommandExecutor;
 import io.github.djxy.permissionManager.subjects.Player;
 import io.github.djxy.permissionManager.subjects.Subject;
 import org.spongepowered.api.command.CommandSource;
-import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.serializer.TextSerializers;
 
 import java.util.Map;
@@ -27,7 +28,7 @@ public class SubjectSetPrefixExecutor extends CommandExecutor {
         String prefix = (String) values.get("prefix");
         String name = subject instanceof Player ? PlayerRepository.getInstance().getPlayerName(UUID.fromString(subject.getIdentifier())):subject.getIdentifier();
 
-        source.sendMessage(PREFIX.concat(Text.of("The prefix of ", INFO_COLOR, name, RESET_COLOR, " is now ").concat(TextSerializers.FORMATTING_CODE.deserialize(prefix+"&f").concat(Text.of(".")))));
+        source.sendMessage(Main.getTranslatorInstance().translate(source, "subjectSetPrefix", CoreUtil.createMap("subject", name, "prefix", TextSerializers.FORMATTING_CODE.deserialize(prefix + "&f"))));
 
         subject.setPrefix(prefix);
     }
