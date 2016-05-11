28b8830e0e7a9393ecf0ec2cc2defdade7d3c93e
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-05-11 11:00:40 -0400

    NEw value

--- src/main/java/io/github/djxy/permissionManager/commands/executors/SubjectRemovePermissionWorldExecutor.java
+++ src/main/java/io/github/djxy/permissionManager/commands/executors/SubjectRemovePermissionWorldExecutor.java
@@ -1,15 +1,17 @@
 package io.github.djxy.permissionManager.commands.executors;
 
+import io.github.djxy.core.CoreUtil;
 import io.github.djxy.core.repositories.PlayerRepository;
+import io.github.djxy.permissionManager.Main;
 import io.github.djxy.permissionManager.Permissions;
 import io.github.djxy.permissionManager.commands.CommandExecutor;
 import io.github.djxy.permissionManager.subjects.Permission;
 import io.github.djxy.permissionManager.subjects.Player;
 import io.github.djxy.permissionManager.subjects.Subject;
 import org.spongepowered.api.command.CommandSource;
-import org.spongepowered.api.text.Text;
 import org.spongepowered.api.world.World;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -27,19 +29,24 @@ public class SubjectRemovePermissionWorldExecutor extends CommandExecutor {
         String permission = (String) values.get("permission");
         String worldName =((World) values.get("world")).getName();
         Permission perm = subject.getPermission(worldName, permission);
+        String name = subject instanceof Player ?PlayerRepository.getInstance().getPlayerName(((Player) subject).getUUID()):subject.getIdentifier();
 
         if(perm != null) {
-            String name = subject instanceof Player ? PlayerRepository.getInstance().getPlayerName(((Player) subject).getUUID()):subject.getIdentifier();
-
-            source.sendMessage(PREFIX.concat(Text.of(INFO_COLOR, name, RESET_COLOR, " no longer has the permission ", INFO_COLOR, permission, RESET_COLOR, " in the world ", INFO_COLOR, worldName, RESET_COLOR,".")));
-
+            source.sendMessage(Main.getTranslatorInstance().translate(source, "subjectRemovePermissionWorld", createMap("subject", name, "permission", permission, "world", worldName)));
             subject.removePermission(worldName, permission);
         }
-        else {
-            String name = subject instanceof Player ?PlayerRepository.getInstance().getPlayerName(((Player) subject).getUUID()):subject.getIdentifier();
+        else
+            source.sendMessage(Main.getTranslatorInstance().translate(source, "subjectRemovePermissionDoesntHavePerm", CoreUtil.createMap("subject", name, "permission", permission)));
+    }
 
-            source.sendMessage(PREFIX.concat(Text.of(INFO_COLOR, name, RESET_COLOR, " doesn't have the permission ", INFO_COLOR, permission, RESET_COLOR, " in the world ", INFO_COLOR, worldName, RESET_COLOR,".")));
-        }
+    private Map<String,Object> createMap(String key1, Object value1, String key2, Object value2, String key3, Object value3){
+        HashMap<String,Object> map = new HashMap<>();
+
+        map.put(key1, value1);
+        map.put(key2, value2);
+        map.put(key3, value3);
+
+        return map;
     }
 
 }
