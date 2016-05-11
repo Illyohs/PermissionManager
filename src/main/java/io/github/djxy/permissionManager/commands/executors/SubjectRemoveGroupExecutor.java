555b071846a5137bf45f9d6ada80a1ce1913ba2c
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-05-11 10:58:14 -0400

    Translation implemented

--- src/main/java/io/github/djxy/permissionManager/commands/executors/SubjectRemoveGroupExecutor.java
+++ src/main/java/io/github/djxy/permissionManager/commands/executors/SubjectRemoveGroupExecutor.java
@@ -1,13 +1,14 @@
 package io.github.djxy.permissionManager.commands.executors;
 
+import io.github.djxy.core.CoreUtil;
 import io.github.djxy.core.repositories.PlayerRepository;
+import io.github.djxy.permissionManager.Main;
 import io.github.djxy.permissionManager.Permissions;
 import io.github.djxy.permissionManager.commands.CommandExecutor;
 import io.github.djxy.permissionManager.subjects.Group;
 import io.github.djxy.permissionManager.subjects.Player;
 import io.github.djxy.permissionManager.subjects.Subject;
 import org.spongepowered.api.command.CommandSource;
-import org.spongepowered.api.text.Text;
 
 import java.util.Map;
 
@@ -24,27 +25,14 @@ public class SubjectRemoveGroupExecutor extends CommandExecutor {
     public void execute(CommandSource source, Map<String, Object> values) {
         Subject subject = (Subject) values.get("subject");
         Group group = (Group) values.get("group");
+        String name = subject instanceof Player?PlayerRepository.getInstance().getPlayerName(((Player) subject).getUUID()):subject.getIdentifier();
 
         if(subject.hasGroup(group)){
-            if(subject instanceof Player){
-                String name = PlayerRepository.getInstance().getPlayerName(((Player) subject).getUUID());
-
-                source.sendMessage(PREFIX.concat(Text.of(INFO_COLOR, name, RESET_COLOR, " is no longer a member of the group ", INFO_COLOR, group.getIdentifier(), RESET_COLOR, ".")));
-            }
-            else
-                source.sendMessage(PREFIX.concat(Text.of(INFO_COLOR, subject.getIdentifier(), RESET_COLOR, " no longer inherit of the group ", INFO_COLOR, group.getIdentifier(), RESET_COLOR, ".")));
-
+            source.sendMessage(Main.getTranslatorInstance().translate(source, "subjectRemoveGroup", CoreUtil.createMap("subject", name, "group", group.getIdentifier())));
             subject.removeGroup(group);
         }
-        else {
-            if(subject instanceof Player){
-                String name = PlayerRepository.getInstance().getPlayerName(((Player) subject).getUUID());
-
-                source.sendMessage(PREFIX.concat(Text.of(INFO_COLOR, name, RESET_COLOR, " is not a member of the group ", INFO_COLOR, group.getIdentifier(), RESET_COLOR, ".")));
-            }
-            else
-                source.sendMessage(PREFIX.concat(Text.of(INFO_COLOR, subject.getIdentifier(), RESET_COLOR, " doesn't inherit of the group ", INFO_COLOR, group.getIdentifier(), RESET_COLOR, ".")));
-        }
+        else
+            source.sendMessage(Main.getTranslatorInstance().translate(source, "subjectRemoveGroupDoesntHaveGroup", CoreUtil.createMap("subject", name, "group", group.getIdentifier())));
     }
 
 }
