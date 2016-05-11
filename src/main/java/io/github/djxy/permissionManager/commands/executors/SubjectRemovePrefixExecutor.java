490373a170dbe5fe412551a0507b8f059f03bd8c
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-05-11 10:57:37 -0400

    Translation implemented

--- src/main/java/io/github/djxy/permissionManager/commands/executors/SubjectRemovePrefixExecutor.java
+++ src/main/java/io/github/djxy/permissionManager/commands/executors/SubjectRemovePrefixExecutor.java
@@ -1,12 +1,13 @@
 package io.github.djxy.permissionManager.commands.executors;
 
+import io.github.djxy.core.CoreUtil;
 import io.github.djxy.core.repositories.PlayerRepository;
+import io.github.djxy.permissionManager.Main;
 import io.github.djxy.permissionManager.Permissions;
 import io.github.djxy.permissionManager.commands.CommandExecutor;
 import io.github.djxy.permissionManager.subjects.Player;
 import io.github.djxy.permissionManager.subjects.Subject;
 import org.spongepowered.api.command.CommandSource;
-import org.spongepowered.api.text.Text;
 
 import java.util.Map;
 import java.util.UUID;
@@ -25,7 +26,7 @@ public class SubjectRemovePrefixExecutor extends CommandExecutor {
         Subject subject = (Subject) values.get("subject");
         String name = subject instanceof Player ? PlayerRepository.getInstance().getPlayerName(UUID.fromString(subject.getIdentifier())):subject.getIdentifier();
 
-        source.sendMessage(PREFIX.concat(Text.of(INFO_COLOR, name, RESET_COLOR, " no longer has a prefix.")));
+        source.sendMessage(Main.getTranslatorInstance().translate(source, "subjectRemovePrefix", CoreUtil.createMap("subject", name)));
 
         subject.setPrefix("");
     }
